cc908e36b9ff63fe28d000d6b3ee56ff3ca950b7
AxelGarciaTello <gata21022000@hotmail.com>
2021-04-20 11:33:56 -0500

    Clase copiada

--- GUI/EvaluadorExpresion.py
+++ GUI/EvaluadorExpresion.py
@@ -9,7 +9,8 @@ class EvaluadorExpresion:
     def IniEval(self):
         v = 0
         token = 0
-        if self.E(v):
+        sino, v = self.E(v)
+        if sino:
             token = self.analizadores[0].yylex()
             if token == "Fin":
                 self.resultado = v
@@ -18,10 +19,12 @@ class EvaluadorExpresion:
         return False
 
     def E(self, v):
-        if self.T(v):
-            if self.Ep(v):
-                return True
-        return False
+        sino, v = self.T(v)
+        if sino:
+            sino, v = self.Ep(v)
+            if sino:
+                return True, v
+        return False, v
 
     def Ep(self, v):
         token = 0
@@ -29,23 +32,27 @@ class EvaluadorExpresion:
         aux = 0.0
         token = self.analizadores[0].yylex()
         if token == 10 or token == 20:
-            if self.T(v2):
+            sino, v2 = self.T(v2)
+            if sino:
                 if token == 10:
                     aux = v2
                 else:
                     aux = -v2
                 v = v + aux
-                if self.Ep(v):
-                    return True
-            return False
+                sino, v = self.Ep(v)
+                if sino:
+                    return True, v
+            return False, v
         self.analizadores[0].undoToken()
-        return True
+        return True, v
 
     def T(self, v):
-        if self.F(v):
-            if self.Tp(v):
-                return True
-        return False
+        sino, v = self.F(v)
+        if sino:
+            sino, v = self.Tp(v)
+            if sino:
+                return True, v
+        return False, v
 
     def Tp(self, v):
         token = 0
@@ -53,31 +60,34 @@ class EvaluadorExpresion:
         aux = 0.0
         token = self.analizadores[0].yylex()
         if token == 30 or token == 40:
-            if self.F(v2):
+            sino, v2 = self.F(v2)
+            if sino:
                 if token == 30:
                     aux = v2
                 else:
                     aux = 1/v2
                 v = v * aux
-                if self.Tp(v):
-                    return True
-            return False
+                sino, v = self.Tp(v)
+                if sino:
+                    return True, v
+            return False, v
         self.analizadores[0].undoToken()
-        return True
+        return True, v
 
     def F(self, v):
         token = 0
         token = self.analizadores[0].yylex()
         if token == 50:
-            if self.E(v):
+            sino, v = self.E(v)
+            if sino:
                 token = self.analizadores[0].yylex()
                 if token == 60:
-                    return True
-            return False
+                    return True, v
+            return False, v
         elif token == 70:
             v = float(self.analizadores[0].miLexema())
-            return True
-        return False
+            return True, v
+        return False, v
 
     def getResultado(self):
         return self.resultado
